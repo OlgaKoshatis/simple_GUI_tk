d13f5a7861701b620a50bc026019dbfd81366d25
имя латинскими буквами <  >
2023-12-11 13:49:33 +0300

    added two def about speed

--- code.py
+++ code.py
@@ -1,4 +1,14 @@
 from tkinter import *
+from speedtest import Speedtest
+
+def test():
+    download = Speedtest().download()
+    upload = Speedtest.upload()
+    download_speed = round(download / (10**6), 2)
+    upload_speed = round(upload / (10**6), 2)
+
+    download_label.config(text="Скорость загрузки:\n" + str(download_speed) + "MbPs")
+    upload_label.config(text="Скорость отдачи:\n" + str(upload_speed) + "MbPs")
 
 root = Tk()
 
@@ -9,9 +19,9 @@ button = Button(root, text='Click me', width=15, height=2, font=('Arial', 20), b
 button.pack(side=BOTTOM, pady=40)
 
 download_label = Label(root, text='Скорость загрузки:\n-', font=('Arial', 20))
-download_label.pack(side=TOP, pady=40)
+download_label.pack(side=TOP, pady=(50, 0))
 upload_label = Label(root, text='Скорость отдачи:\n-', font=('Arial', 20))
-upload_label.pack(side=TOP, pady=40)
+upload_label.pack(side=TOP, pady=(10, 0))
 
 
 root.mainloop()
