449ccd11ea1f568c749912dbcc2c94c2933d3fb5
Olga <ahethyfy@gmail.com>
2023-12-11 15:29:02 +0300

    color changes number 2

--- code.py
+++ code.py
@@ -18,10 +18,10 @@ root.title('SpeedTest')
 root.geometry('300x400')
 
 button = Button(root, text='Click me', width=15, height=2, font=('Arial', 20), borderwidth=5, relief=GROOVE)
-button.pack(side=BOTTOM, pady=40, bg='SteelBlue', fg='Silver')
+button.pack(side=BOTTOM, pady=40,)
 
 download_label = Label(root, text='Скорость загрузки:\n-', font=('Arial', 20))
-download_label.pack(side=TOP, pady=(50, 0), bg='SteelBlue', fg='Silver')
+download_label.pack(side=TOP, pady=(50, 0))
 upload_label = Label(root, text='Скорость отдачи:\n-', font=('Arial', 20))
 upload_label.pack(side=TOP, pady=(10, 0))
 
