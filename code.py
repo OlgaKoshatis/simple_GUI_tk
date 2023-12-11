9d9b7ea29c342d3d34bc31b1f25ad4de8f517baa
Olga <ahethyfy@gmail.com>
2023-12-11 15:27:46 +0300

    color changes number one

--- code.py
+++ code.py
@@ -8,8 +8,8 @@ def test():
     download_speed = round(download / (10 ** 6), 2)
     upload_speed = round(upload / (10 ** 6), 2)
 
-    download_label.config(text="Скорость загрузки:\n" + str(download_speed) + 'Mb/s')
-    upload_label.config(text="Скорость отдачи:\n" + str(upload_speed) + "MbPs")
+    download_label.config(text="Скорость загрузки:\n" + str(download_speed) + 'Mbit/s')
+    upload_label.config(text="Скорость отдачи:\n" + str(upload_speed) + "Mbit/s")
 
 
 root = Tk()
@@ -17,11 +17,11 @@ root = Tk()
 root.title('SpeedTest')
 root.geometry('300x400')
 
-button = Button(root, text='Click me', width=15, height=2, font=('Arial', 20), bg='MidnightBlue', fg='yellow', borderwidth=5, relief=GROOVE)
-button.pack(side=BOTTOM, pady=40)
+button = Button(root, text='Click me', width=15, height=2, font=('Arial', 20), borderwidth=5, relief=GROOVE)
+button.pack(side=BOTTOM, pady=40, bg='SteelBlue', fg='Silver')
 
 download_label = Label(root, text='Скорость загрузки:\n-', font=('Arial', 20))
-download_label.pack(side=TOP, pady=(50, 0))
+download_label.pack(side=TOP, pady=(50, 0), bg='SteelBlue', fg='Silver')
 upload_label = Label(root, text='Скорость отдачи:\n-', font=('Arial', 20))
 upload_label.pack(side=TOP, pady=(10, 0))
 
